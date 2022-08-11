8a3be5ee754e69cdc26477c46412e3e30f39cea5
Sebastien Noret <sebastien.noret@gmail.com>
2022-08-11 23:57:08 +0200

    finish create relative links , try to add flash message to contact page

--- src/index.js
+++ src/index.js
@@ -2,21 +2,26 @@ if (document.getElementById("message-form")) {
     const userName = document.getElementById("name");
     const email = document.getElementById("email");
     const message = document.getElementById("message");
+    const flash= document.getElementById("flash-message")
     document
       .getElementById("message-form")
       .addEventListener("submit", function (event) {
         event.preventDefault();
-        alert("Votre message a bien été envoyé.");
+        event.stopPropagation();
+        
+        // alert("Votre message a bien été envoyé.");
+ flash.classList.toggle("js-display");
+ setTimeout(()=>flash.classList.toggle("js-display"),5000)
         email.value = "";
         userName.value = "";
         message.value = "";
       });
   }
   
-  document
-    .getElementById("menu-button")
-    .addEventListener("click", function (event) {
-      const menu = document.getElementById("navlist");
-      menu.classList.toggle("js-menu");
-    });
+  // document
+  //   .getElementById("menu-button")
+  //   .addEventListener("click", function (event) {
+  //     const menu = document.getElementById("navlist");
+  //     menu.classList.toggle("js-menu");
+  //   });
   
