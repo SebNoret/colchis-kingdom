fbe2aceb1e07c0d788f4ca114a5b2a02de157cb4
Sebastien Noret <sebastien.noret@gmail.com>
2022-08-13 19:40:54 +0200

    add a modal popup on contact page

--- src/index.js
+++ src/index.js
@@ -1,27 +1,55 @@
 if (document.getElementById("message-form")) {
-    const userName = document.getElementById("name");
-    const email = document.getElementById("email");
-    const message = document.getElementById("message");
-    const flash= document.getElementById("flash-message")
-    document
-      .getElementById("message-form")
-      .addEventListener("submit", function (event) {
-        event.preventDefault();
-        event.stopPropagation();
-        
-        alert("Votre message a bien été envoyé.");
-//  flash.classList.toggle("js-display");
-//  setTimeout(()=>flash.classList.toggle("js-display"),5000)
-//         email.value = "";
-//         userName.value = "";
-//         message.value = "";
-      });
-  }
-  
+  const userName = document.getElementById("name");
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
+  // Get the modal
+  const modal = document.getElementById("modal-message");
   document
-    .getElementById("menu-button")
-    .addEventListener("click", function (event) {
-      const menu = document.getElementById("navlist");
-      menu.classList.toggle("js-menu");
+    .getElementById("message-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      modal.style.display = "flex";
+      email.value = "";
+      userName.value = "";
+      message.value = "";
+      setTimeout(() => {
+        modal.style.display = "none";
+      }, 5000)
     });
-  
+}
+
+document
+  .getElementById("menu-button")
+  .addEventListener("click", function (event) {
+    const menu = document.getElementById("navlist");
+    menu.classList.toggle("js-menu");
+  });
+
+
+// Get the modal
+// var modal = document.getElementById("modal-message");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//   modal.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
