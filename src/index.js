48d780171061d49040eaf9aaece4bfcf0b6089aa
Sebastien Noret <sebastien.noret@gmail.com>
2022-08-14 17:20:56 +0200

    repair responsive menu

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ if (document.getElementById("message-form")) {
   const userName = document.getElementById("name");
   const email = document.getElementById("email");
   const message = document.getElementById("message");
+
+
   // Get the modal
   const modal = document.getElementById("modal-message");
   document
@@ -10,16 +12,19 @@ if (document.getElementById("message-form")) {
       event.preventDefault();
       event.stopPropagation();
 
-      // setTimeout(() => {
+      setTimeout(() => {
 
-      //   modal.style.display = "flex";
-      // }, 600);
+        modal.style.display = "flex";
+        document.body.style.overflow = "hidden";
+      }, 600);
       email.value = "";
       userName.value = "";
       message.value = "";
-      // setTimeout(() => {
-      //   modal.style.display = "none";
-      // }, 3500)
+      setTimeout(() => {
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+
+      }, 3500)
     });
 }
 
@@ -31,28 +36,3 @@ document
   });
 
 
-// Get the modal
-// var modal = document.getElementById("modal-message");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// btn.onclick = function () {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function () {
-//   modal.style.display = "none";
-// }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
