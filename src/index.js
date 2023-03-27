b9915d9c461d6184e35e04620c5d766ad5ffaa59
Sebastien Noret <sebastien.noret@gmail.com>
2023-03-27 15:20:08 +0200

    change file path to tresor royal, add some content, delete preloader from pages

--- src/index.js
+++ src/index.js
@@ -30,19 +30,3 @@ document
     const menu = document.getElementById("navlist");
     menu.classList.toggle("js-menu");
   });
-
-// preloader
-
-const preloader = document.getElementById("preloader");
-const body = document.body;
-window.addEventListener("load", function () {
-  const timer = setTimeout(() => {
-    if (body.classList.contains("js-loading")) {
-      body.classList.remove("js-loading");
-    }
-    preloader.style.opacity = 0;
-    //preloader.style.display = "none";
-
-    clearTimeout(timer);
-  }, 2000);
-});
